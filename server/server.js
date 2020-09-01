a1bd9e5049b26437b375126e27e833f410e277eb
fs77x <farhansaeed1999@gmail.com>
2020-08-31 18:10:43 -0700

    serverless still not working oof

--- server/server.js
+++ server/server.js
@@ -22,59 +22,59 @@ const PORT = 8080
 
 //Server: construct an express webserver based on the specifications
 //        that are required (all this is handled in the class)
-class Server{
+// class Server{
 
-   constructor(){
-      this.app = express()
+//    constructor(){
+app = express()
 
-      // all imported libraries that the server takes advantage of
-      this.app.use(fileUpload({
-         createParentPath: true
-      }));
-      this.app.use(cors())
-      this.app.use(bodyParser.json())
-      this.app.use(bodyParser.urlencoded({extended: true}))
-      this.app.use(morgan("dev"))
+// all imported libraries that the server takes advantage of
+app.use(fileUpload({
+   createParentPath: true
+}));
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(morgan("dev"))
 
-      // route serves to check if the server is up and running
-      this.app.get("/", function (req, res) {
-         res.sendStatus(200)
-      })
+// route serves to check if the server is up and running
+app.get("/", function (req, res) {
+   res.sendStatus(200)
+})
 
-      // purpose: return the classes, professor, or both to our client
-      this.app.get("/send_data", async (req, res) => {
-         await send_data.send_data(req, res)
-      })
+// purpose: return the classes, professor, or both to our client
+app.get("/send_data", async (req, res) => {
+   await send_data.send_data(req, res)
+})
 
-      // purpose: handles uploading files + folder creations (if DNE)
-      this.app.post("/upload_file",async(req,res)=>{
-         await upload_file.upload_file(req,res)
-      })
+// purpose: handles uploading files + folder creations (if DNE)
+app.post("/upload_file",async(req,res)=>{
+   await upload_file.upload_file(req,res)
+})
 
-      // purpose: return all class codes for client prevents users 
-      // typing in wrong codes i.e CS instead of CSE
-      this.app.get("/send_class_codes", async(req,res) =>{
-         await send_class_codes.send_class_codes(req,res)
-      })
+// purpose: return all class codes for client prevents users 
+// typing in wrong codes i.e CS instead of CSE
+app.get("/send_class_codes", async(req,res) =>{
+   await send_class_codes.send_class_codes(req,res)
+})
 
-      // purpose: return the ratings for the documents of a specific
-      // professor and class
-      this.app.get("/get_rating", async(req, res) => {
-         await rating.get_rating(req, res)
-      })
+// purpose: return the ratings for the documents of a specific
+// professor and class
+app.get("/get_rating", async(req, res) => {
+   await rating.get_rating(req, res)
+})
 
-      // purpose: checks if user email has ucsc.edu domain
-      this.app.post("/verify_email", async(req, res) => {
-         await verify.verify_email(req, res)
-      })
-
-
+// purpose: checks if user email has ucsc.edu domain
+app.post("/verify_email", async(req, res) => {
+   await verify.verify_email(req, res)
+})
 
 
-   }
 
 
-}
+//    }
+
+
+// }
 
 //below constructs and starts our server
-module.exports = Server;
+module.exports = app;
